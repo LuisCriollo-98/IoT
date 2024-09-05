c0331c8e3481eb59398798a5d75a109c5a3e3868
Alejandro <lucho199802@gmail.com>
2024-09-04 20:15:07 -0500

    Actualizando repo 2_09_2024

---
+++ python/scripts_python/api_nasa1.py
@@ -0,0 +1,8 @@
+'''
+API: Application programing interface
+Nasa AP: https://api.nasa.gov
+Develope: Luis C.
+
+Ruta de api 
+https://api.nasa.gov/neo/rest/v1/neo/3726712?api_key={iMEuq93q7ddL7WzhQXv1mjrZkld3JEfpsxmCG6nT}
+'''
